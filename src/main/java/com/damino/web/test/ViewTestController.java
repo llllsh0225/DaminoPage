d3eb7ae66d71b7316fcfabeb02c80da875b66a13
kjh <kjh456879@naver.com>
2020-07-21 17:26:41 +0900

    컨트롤러 수정

--- src/main/java/com/damino/web/test/ViewTestController.java
+++ src/main/java/com/damino/web/test/ViewTestController.java
@@ -2,13 +2,26 @@ package com.damino.web.test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class ViewTestController {
 	@RequestMapping("/main.do")
-	public String getMainPage() {
+	public ModelAndView getMainPage() {
 		System.out.println("메인페이지 열기");
 		
-		return "main";
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/main");
+		
+		return mav;
+	}
+	@RequestMapping("/quickOrdermain.do")
+	public ModelAndView getQuickOrderPage() {
+		System.out.println("퀵오더페이지 열기");
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/quickorder/quickOrdermain");
+		
+		return mav;
 	}
 }
