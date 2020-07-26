65cf48d03d2b2e1049b6fa82b1c5a045fbb4c756
kjhproject <kjh78951@gmail.com>
2020-07-26 23:25:00 +0900

    Revert "게시판 DB 불러오기"
    This reverts commit ad23c632fff4434c52a1b487d51ad6899a21e3f8.

--- src/main/java/com/damino/web/admin/board/BoardDAO.java
+++
@@ -1,7 +0,0 @@
-package com.damino.web.admin.board;
-
-import java.util.List;
-
-public interface BoardDAO {
-	public List<BoardVO> getBoardList();
-}
--- src/main/java/com/damino/web/admin/board/BoardService.java
+++
@@ -1,7 +0,0 @@
-package com.damino.web.admin.board;
-
-import java.util.List;
-
-public interface BoardService {
-	public List<BoardVO> getBoardList();
-}
