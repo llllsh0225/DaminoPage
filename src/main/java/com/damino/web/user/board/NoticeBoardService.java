83590bbe8b2271b5111619d92511c56707dddf37
kjh <kjh456879@naver.com>
2020-07-31 13:01:56 +0900

    홈페이지 관리자 게시판관리 수정

--- src/main/java/com/damino/web/user/board/NoticeBoardService.java
+++ src/main/java/com/damino/web/user/board/NoticeBoardService.java
@@ -2,11 +2,13 @@ package com.damino.web.user.board;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import com.damino.web.user.board.paging.Paging;
 
 public interface NoticeBoardService {
 	public List<NoticeBoardVO> getNoticeBoardList(Paging pa);//공지사항 목록
 	public NoticeBoardVO getNoticeBoard(NoticeBoardVO vo);//공지사항 상세보기
 	public int countNoticeBoard();//공지사항 게시글 수
-	public void increaseCnt(NoticeBoardVO vo);//공지사항 조회수
+	public void increaseCnt(NoticeBoardVO vo, HttpSession session);//공지사항 조회수
 }
