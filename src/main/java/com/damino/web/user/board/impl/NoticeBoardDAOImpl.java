36abd3b7a9211717a8927138d250a5210e4f1f2c
kjh <kjh456879@naver.com>
2020-07-31 11:19:55 +0900

    공지사항 게시판 페이징 처리 기능 추가

--- src/main/java/com/damino/web/user/board/impl/NoticeBoardDAOImpl.java
+++ src/main/java/com/damino/web/user/board/impl/NoticeBoardDAOImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import com.damino.web.user.board.NoticeBoardDAO;
 import com.damino.web.user.board.NoticeBoardVO;
-import com.damino.web.user.board.NoticePagingVO;
+import com.damino.web.user.board.paging.Paging;
 
 @Repository("noticeBoardDAO")
 public class NoticeBoardDAOImpl implements NoticeBoardDAO {
@@ -17,16 +17,16 @@ public class NoticeBoardDAOImpl implements NoticeBoardDAO {
 	
 	//공지사항 전체목록
 	@Override
-	public List<NoticeBoardVO> getNoticeBoardList(NoticePagingVO vo) {
-		System.out.println("NoticeBoardDAOImpl getNoticeBoardList(vo)");
-		return sqlSessionTemplate.selectList("BoardDAO.getNoticeBoardList", vo);
+	public List<NoticeBoardVO> getNoticeBoardList(Paging pa) {
+		System.out.println("NoticeBoardDAOImpl getNoticeBoardList(pa)");
+		return sqlSessionTemplate.selectList("BoardDAO.getBoardList", pa);
 	}
 	
 	//공지사항 상세보기
 	@Override
 	public NoticeBoardVO getNoticeBoard(NoticeBoardVO vo) {
 		System.out.println("NoticeBoardDAOImpl getNoticeBoard(vo)");
-		return sqlSessionTemplate.selectOne("BoardDAO.noticePaging", vo);
+		return sqlSessionTemplate.selectOne("BoardDAO.getNoticeBoard", vo);
 	}
 	
 	//공지사항 전체 게시글 수 
