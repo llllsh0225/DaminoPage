a81568315145e6ddc7cefe878ebfa4c7b0a1f44b
jeong <jeongsam3659@naver.com>
2020-07-31 22:59:33 +0900

    <id중복체크>
    id중복체크를
    usermember쪽으로 옮기고 실행하였습니다.

--- src/main/java/com/damino/web/user/regist/RegistService.java
+++ src/main/java/com/damino/web/user/regist/RegistService.java
@@ -4,7 +4,9 @@ public interface RegistService {
 	public void registMember(UserMemberVO vo);
 	public void certifiedPhoneNumber(String phoneNumber, String authKey);
 	
+	public int idcheck(String userid);
 	public int emailcheck(String email);
+	
 	
 	
 }
