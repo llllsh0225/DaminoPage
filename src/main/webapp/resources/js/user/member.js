e0d0dc9ed8921c0263ebe0fdbc7d81f6fa0c9c2c
SuHyeon Oh <romantico_u@naver.com>
2020-07-31 10:40:48 +0900

    회원가입 구현

--- src/main/webapp/resources/js/user/member.js
+++ src/main/webapp/resources/js/user/member.js
@@ -23,7 +23,7 @@ function checks(){
 		$("#username").focus();
 		return false;
 	}
-/*	
+	
 	//아이디 공백 확인
 	if($("#userid").val() == ""){
 		alert("아이디를 입력해주세요.");
@@ -38,7 +38,7 @@ function checks(){
 		$("#userid").focus(); 
 		return false; 
 	}
-*/	
+	
 	//비밀번호 공백 확인 
 	if($("#userpasswd").val() == ""){ 
 		alert("패스워드를 입력하세요"); 
@@ -62,19 +62,40 @@ function checks(){
 		$("#userpasswd").focus(); 
 		return false; 
 	}
+	
+	if($("#phoneCheck").val == "N"){
+		alert("핸드폰번호를 인증해주세요.");
+		return false;
+	}
 
 }
 	
-	
-/**
- * 약관동의
-  
-	 $(document).ready(function(){
-	    $('.all-check').click( function(){
-	        $('#location_yn','#agree_1','#agree_2').prop('checked', this.checked);
-	    });
+
+$(document).ready(function(){
+	$('#agree_all1').click(function(){
+		if($('#agree_all1').prop('checked')){
+			$('#location_yn').prop('checked', true);
+			$('#agree_1').prop('checked', true);
+			$('#agree_2').prop('checked', true);
+		}else{
+			$('#location_yn').prop('checked', false);
+			$('#agree_1').prop('checked', false);
+			$('#agree_2').prop('checked', false);
+		}
 	});
-*/
+	$('#agree_all2').click(function(){
+		if($('#agree_all2').prop('checked')){
+			$('#chk_ds_fl').prop('checked', true);
+			$('#chk_dm_fl').prop('checked', true);
+			$('#chk_o_dm_fl').prop('checked', true);
+		}else{
+			$('#chk_ds_fl').prop('checked', false);
+			$('#chk_dm_fl').prop('checked', false);
+			$('#chk_o_dm_fl').prop('checked', false);
+		}
+	});
+});
+
 
 
 /**
@@ -92,68 +113,5 @@ function checkEmailState(obj, obj2) {
 	} else {
 		obj2.val(obj.val());
 		obj2.attr("readOnly", true);
-	}
-}
-
-
-
-/**
- * 인증번호 발송
- *
- * @param auth_type
- * @param email
- * @param hand_tel1
- * @param hand_tel2
- * @param hand_tel3
- * @return
- */
-var memberAuthSendChk = false;
-function memberAuthSend(auth_type, email, hand_tel1, hand_tel2, hand_tel3, gubun_type) {
-	var errorTxt = "인증번호 발송 중 오류가 발생되었습니다. 다시 시도해주세요.";
-
-	if(!memberAuthSendChk){
-		memberAuthSendChk = true;
-		$.ajax({
-			type: "POST",
-			url: "/member/authSendAjax",
-			data: { "auth_type" : auth_type,
-					"email" : email,
-					"hand_tel1" : hand_tel1,
-					"hand_tel2" : hand_tel2,
-					"hand_tel3" : hand_tel3	 },
-			dataType : "json",
-			success:function(data) {
-				if (data.status == "success") {
-					$("#hdnAuthKey").val(data.msg);
-					alert("인증번호가 발송되었습니다.\n인증번호 입력란에 수신된 인증번호를 입력해 주세요.");
-				} else if (data.status == "hpError") {
-					alert("전화번호 형태가 잘못 되었습니다.");
-					if (gubun_type == "Y")
-						showMemberInfo();
-				} else if (data.status == "fail") {
-					if (data.msg == "1min") {
-						alert("인증번호 재발송은 발송 후 1분 후 가능합니다.");
-					} else if (data.msg == "1day") {
-						alert("인증번호 발송은 하루에 5회로 제한됩니다.");
-					} else if (data.msg == "PCID ERROR") {
-						alert("비정상적 접근입니다.");
-					} 
-
-					if (gubun_type == "Y")
-						showMemberInfo();
-				} else if (data.status == "error") { // SMS 모듈 오류
-					alert(errorTxt);
-				} else {
-					alert("입력하신 정보가 불충분합니다 확인바랍니다.");
-					if (gubun_type == "Y")
-						showMemberInfo();
-				}
-				memberAuthSendChk = false;
-			},
-			error: function (error){
-				alert('Error ==> ' + error);
-				memberAuthSendChk = false;
-			}
-		});
 	}
 }
