528c54f5a35e342c7d9520892f1bf9860820a15d
SuHyeon Oh <romantico_u@naver.com>
2020-08-05 13:30:24 +0900

    쿠폰 중복지급 확인, 쿠폰함에 사용 가능 쿠폰 불러오기 구현 (쿠폰 선물하기 구현중)

--- src/main/java/com/damino/web/user/coupon/impl/CouponServiceImpl.java
+++ src/main/java/com/damino/web/user/coupon/impl/CouponServiceImpl.java
@@ -1,5 +1,6 @@
 package com.damino.web.user.coupon.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.damino.web.user.coupon.CouponDAO;
 import com.damino.web.user.coupon.CouponService;
-import com.damino.web.user.login.UserVO;
+import com.damino.web.user.coupon.CouponVO;
 
 @Service("couponService")
 public class CouponServiceImpl implements CouponService {
@@ -15,13 +16,18 @@ public class CouponServiceImpl implements CouponService {
 	private CouponDAO couponDAO;
 	
 	@Override
+	public int chkUsersManiaCoupon(String userid) {
+		return couponDAO.chkUsersManiaCoupon(userid);
+	}
+	
+	@Override
 	public void insertManiaCoupon(Map<String, String> param) {
 		couponDAO.insertManiaCoupon(param);
 	}
 
 	@Override
-	public UserVO getUser(String userid) {
-		return couponDAO.getUser(userid);
+	public List<CouponVO> getMyCouponList(String userid) {
+		return couponDAO.getMyCouponList(userid);
 	}
 
 }
