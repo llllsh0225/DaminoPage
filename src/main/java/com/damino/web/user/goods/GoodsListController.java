c1afaab44fc3b07256460212656137264da7e3de
Soo Hyun <rkdtngus0209@gmail.com>
2020-08-08 14:08:21 +0900

    1. detail_goods.jsp 제품 상세보기 페이지 - 사용자 선택 제품 정보 불러오기 구현(제품명, 제품이미지) 2. list.jsp 주석 내용 삭제

--- src/main/java/com/damino/web/user/goods/GoodsListController.java
+++ src/main/java/com/damino/web/user/goods/GoodsListController.java
@@ -18,24 +18,24 @@ import org.springframework.web.servlet.ModelAndView;
 public class GoodsListController {
 	@Autowired
 	private GoodsListService goodsListService;
-	
-	@RequestMapping("/goodslist.do") 
+
+	@RequestMapping("/goodslist.do")
 	public ModelAndView getPizzaList(ModelAndView mav) {
 		System.out.println("제품 목록 열기");
-		
+
 		List<GoodsPizzaVO> goodsPremiumList = goodsListService.getPizzaList();
 		List<GoodsPizzaVO> goodsClassicList = goodsListService.getPizzaClassicList();
-		
+
 		mav.addObject("goodsPremiumList", goodsPremiumList);
 		mav.addObject("goodsClassicList", goodsClassicList);
-		
+
 		System.out.println("프리미엄 피자 리스트 : " + goodsPremiumList);
 		mav.setViewName("/goods/list");
-		
+
 		return mav;
 	}
-	
-	@RequestMapping("/goodsSideList.do") 
+
+	@RequestMapping("/goodsSideList.do")
 	public ModelAndView getSideList(ModelAndView mav) {
 		System.out.println("사이드디시 리스트 열기");
 
@@ -57,7 +57,7 @@ public class GoodsListController {
 	 * 
 	 * return mav; }
 	 */
-	@RequestMapping("/goodsDrinkEtcList.do") 
+	@RequestMapping("/goodsDrinkEtcList.do")
 	public ModelAndView getDrinkEtcList(ModelAndView mav) {
 		System.out.println("음료&기타 메뉴 열기");
 
@@ -68,26 +68,44 @@ public class GoodsListController {
 
 		return mav;
 	}
+
 	
-	/*
-	 * @RequestMapping(value ="/goods/detailSlide.do", method = RequestMethod.GET)
-	 * public String goodsDetail(@ModelAttribute GoodsPizzaVO vo) {
-	 * 
-	 * //서비스에서 p_code, p_name 기준 조회하는 쿼리 제작해야함
-	 * System.out.println("[상품 상세보기 화면 진입 시도] :"); System.out.println("피자 코드 : " +
-	 * vo.getP_code()); System.out.println("피자 이미지 : " + vo.getP_image());
-	 * goodsListService.getUserPizzaGoods(vo); return "redirect:goodslist.do"; }
-	 */
-	
-	@RequestMapping(value = "/detail.do", method = RequestMethod.GET)
-	@ResponseBody
-	public ModelAndView goView(ModelAndView mav,@ModelAttribute GoodsPizzaVO vo, HttpServletRequest request) {
-		System.out.println("사용자 선택 피자메뉴 열기");
+	@RequestMapping(value = "/detailSlide.do", method = RequestMethod.GET)
+	public String goodsDetail(@ModelAttribute GoodsPizzaVO vo) {
 		
-		String code = vo.getP_code();
-		String name = vo.getP_name();
-		System.out.println("code" + code);
-		System.out.println("name" + name);
+		// 서비스에서 p_code, p_name 기준 조회하는 쿼리 제작해야함
+		System.out.println("detailSlide.do");
+		System.out.println("[상품 상세보기 화면 진입 시도] :");
+		System.out.println("피자 코드 : " + vo.getP_code());
+		System.out.println("피자 이미지 : " + vo.getP_image());
+		
+		goodsListService.getUserPizzaGoods(vo);
+		
+		return "redirect:goodslist.do";
+	}
+
+	@RequestMapping(value = "/detailAjax.do", method = RequestMethod.GET)
+	public String goodsDetailAjax(@ModelAttribute GoodsPizzaVO vo) {
+
+		// 서비스에서 p_code, p_name 기준 조회하는 쿼리 제작해야함
+		System.out.println("[상품 상세보기 화면 진입 시도] :");
+		System.out.println("피자 코드 : " + vo.getP_code());
+		System.out.println("피자 이미지 : " + vo.getP_image());
+		
+		goodsListService.getUserPizzaGoods(vo);
+		
+		return "redirect:goodslist.do";
+	}
+	
+	@RequestMapping("/detail.do")
+	public ModelAndView goView(ModelAndView mav, HttpServletRequest request, @ModelAttribute GoodsPizzaVO vo) {
+		System.out.println("사용자 선택 피자메뉴 열기");
+
+		
+		String p_code = request.getParameter("p_code");
+		System.out.println("p_code : " + p_code);
+		String p_name = request.getParameter("p_name");
+		//String p_image = request.getParameter("p_image");
 		
 		GoodsPizzaVO goodsDetail = goodsListService.getUserPizzaGoods(vo);
 		mav.addObject("goodsDetail", goodsDetail);
@@ -95,5 +113,5 @@ public class GoodsListController {
 
 		return mav;
 	}
-	
+
 }
