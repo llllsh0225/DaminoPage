7b0d095cf201273892757cc1d0af0b1247636fb0
minsik <minsik15@naver.com>
2020-08-19 20:15:12 +0900

    비회원 로그인 구현중

--- src/main/java/com/damino/web/user/login/LoginController.java
+++ src/main/java/com/damino/web/user/login/LoginController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -75,6 +77,7 @@ public class LoginController {
 		mav.addObject("username", null);
 		mav.setViewName("main");
 		return mav;
-		
 	}
+	
+	
 }
