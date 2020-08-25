9cd3d105b117129bb789f6fbe539298cfdd3aaa8
jeong <jeongsam3659@naver.com>
2020-08-25 17:07:54 +0900

    <viewcontroller 건든분만 해당> <board-mapping, board 관련분 해당> 홈페이지관리자 admin.admdo
    공지시항 링크 및 이동 연결

--- src/main/java/com/damino/web/admin/board/impl/BoardDAOImpl.java
+++ src/main/java/com/damino/web/admin/board/impl/BoardDAOImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.damino.web.admin.board.BoardDAO;
 import com.damino.web.admin.board.BoardVO;
 
+
 @Repository("boardDAO")
 public class BoardDAOImpl implements BoardDAO {
 	@Autowired
@@ -43,6 +44,13 @@ public class BoardDAOImpl implements BoardDAO {
 	public BoardVO getBoard(BoardVO vo) {
 		System.out.println("BoardDAOImpl getBoard(vo)");
 		return sqlSessionTemplate.selectOne("BoardDAO.getBoard", vo);
+	}
+
+	/* main 홈페이지용 */
+	@Override
+	public List<BoardVO> BoardListMain() {
+		System.out.println(" BoardListmain [dao]");
+		return sqlSessionTemplate.selectList("BoardDAO.getBoardListMain");
 	}
 
 	
