20b962d61ef186d091547093f1c8a24909bbb98b
SuHyeon Oh <romantico_u@naver.com>
2020-09-03 17:55:01 +0900

    에러페이지처리 방식 변경 (관리자페이지 수정중)

--- src/main/java/com/damino/web/user/advice/UserpageAdvice.java
+++ src/main/java/com/damino/web/user/advice/UserpageAdvice.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class UserpageAdvice {
 
+	/**
 	@ExceptionHandler(Exception.class)
 	public String errorPage(Exception e) {
 		e.printStackTrace();
 		
 		return "error";
 	}
+	*/
 }
