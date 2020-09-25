cb8eeaedc24c865908289606a3029cb80b97aa5d
강-PC\강 <rkdtngus0209@gmail.com>
2020-09-25 17:07:01 +0900

    no message

---
+++ src/main/java/board/BoardService.java
@@ -0,0 +1,16 @@
+package com.damino.web.admin.board;
+
+import java.util.List;
+
+
+
+public interface BoardService {
+	public void insertBoard(BoardVO vo);
+	public void updateBoard(BoardVO vo);
+	public void deleteBoard(BoardVO vo);
+	public List<BoardVO> getBoardList();
+	public BoardVO getBoard(BoardVO vo);
+	
+	public List<BoardVO> BoardListMain(); //权其捞瘤包府磊 main侩
+
+}
