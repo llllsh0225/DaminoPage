796dae175e62a9c5881180ba96344c99a9bdf4a1
SuHyeon Oh <romantico_u@naver.com>
2020-09-28 19:11:56 +0900

    통계쿼리 리눅스 환경에 맞도록 수정, BoardController.java 주석 해제

--- src/main/java/com/damino/web/admin/board/BoardController.java
+++ src/main/java/com/damino/web/admin/board/BoardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class BoardController {
 	@Autowired
 	private BoardService boardService;
-	
+
 	/*
 	 * @RequestMapping("/noticeBoardEdit.admdo") public ModelAndView
 	 * getAdminNoticeBoardEditPage() { System.out.println("게시판 리스트 수정 페이지 열기");
@@ -30,42 +30,64 @@ public class BoardController {
 	 * mav.setViewName("/noticeBoard/noticeBoardView");
 	 * 
 	 * return mav; }
-	 * 
-	 * @RequestMapping("/boardList.admdo") public ModelAndView getBoardList(){
-	 * System.out.println("글 목록 "); List<BoardVO> boardList =
-	 * boardService.getBoardList(); System.out.println(boardList.toString());
-	 * ModelAndView mav = new ModelAndView(); mav.setViewName("/board/boardList");
-	 * mav.addObject("boardList", boardList); return mav; }
-	 * 
-	 * @RequestMapping(value="/boardView.admdo", method=RequestMethod.GET) public
-	 * ModelAndView getBoard(BoardVO vo) { System.out.println("글 상세"); BoardVO board
-	 * = boardService.getBoard(vo); ModelAndView mav = new ModelAndView();
-	 * mav.setViewName("/board/boardView"); mav.addObject("board", board); return
-	 * mav; }
-	 * 
-	 * @RequestMapping("/boardWrite.admdo") public ModelAndView
-	 * getAdminBoardWritePage() { System.out.println("게시글 쓰기 열기");
-	 * 
-	 * ModelAndView mav = new ModelAndView(); mav.setViewName("/board/boardWrite");
-	 * 
-	 * return mav; }
-	 * 
-	 * @RequestMapping(value="/updateBoard.admdo", method=RequestMethod.POST) public
-	 * String updateBoard(@ModelAttribute BoardVO vo) { System.out.println("글 수정");
-	 * System.out.println("제목 : " + vo.getTitle()); System.out.println("내용 : " +
-	 * vo.getContent()); boardService.updateBoard(vo); return
-	 * "redirect:boardList.admdo"; }
-	 * 
-	 * @RequestMapping(value="/insertBoard.admdo", method=RequestMethod.POST) public
-	 * String insertBoard(@ModelAttribute BoardVO vo) { System.out.println("글 작성");
-	 * System.out.println("작성자 : " + vo.getWriter()); System.out.println("제목 : " +
-	 * vo.getTitle()); System.out.println("내용 : " + vo.getContent());
-	 * System.out.println("flag : " + vo.getFlag()); boardService.insertBoard(vo);
-	 * //System.out.println(insertBoard(vo).toString());//버그있음 return
-	 * "redirect:boardList.admdo"; }
-	 * 
-	 * @RequestMapping(value="/deleteBoard.admdo", method=RequestMethod.POST) public
-	 * String deleteBoard(@ModelAttribute BoardVO vo) { System.out.println("글 삭제");
-	 * boardService.deleteBoard(vo); return "redirect:boardList.admdo"; }
 	 */
+
+	@RequestMapping("/boardList.admdo")
+	public ModelAndView getBoardList() {
+		System.out.println("글 목록 ");
+		List<BoardVO> boardList = boardService.getBoardList();
+		System.out.println(boardList.toString());
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/board/boardList");
+		mav.addObject("boardList", boardList);
+		return mav;
+	}
+
+	@RequestMapping(value = "/boardView.admdo", method = RequestMethod.GET)
+	public ModelAndView getBoard(BoardVO vo) {
+		System.out.println("글 상세");
+		BoardVO board = boardService.getBoard(vo);
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/board/boardView");
+		mav.addObject("board", board);
+		return mav;
+	}
+
+	@RequestMapping("/boardWrite.admdo")
+	public ModelAndView getAdminBoardWritePage() {
+		System.out.println("게시글 쓰기 열기");
+
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/board/boardWrite");
+
+		return mav;
+	}
+
+	@RequestMapping(value = "/updateBoard.admdo", method = RequestMethod.POST)
+	public String updateBoard(@ModelAttribute BoardVO vo) {
+		System.out.println("글 수정");
+		System.out.println("제목 : " + vo.getTitle());
+		System.out.println("내용 : " + vo.getContent());
+		boardService.updateBoard(vo);
+		return "redirect:boardList.admdo";
+	}
+
+	@RequestMapping(value="/insertBoard.admdo", method=RequestMethod.POST) 
+	public String insertBoard(@ModelAttribute BoardVO vo) { 
+		System.out.println("글 작성");
+		System.out.println("작성자 : " + vo.getWriter()); 
+		System.out.println("제목 : " + vo.getTitle()); System.out.println("내용 : " + vo.getContent());
+		System.out.println("flag : " + vo.getFlag()); boardService.insertBoard(vo);
+		//System.out.println(insertBoard(vo).toString());//버그있음 return
+		return "redirect:boardList.admdo"; 
+	}
+	 
+
+	@RequestMapping(value = "/deleteBoard.admdo", method = RequestMethod.POST)
+	public String deleteBoard(@ModelAttribute BoardVO vo) {
+		System.out.println("글 삭제");
+		boardService.deleteBoard(vo);
+		return "redirect:boardList.admdo";
+	}
+
 }
